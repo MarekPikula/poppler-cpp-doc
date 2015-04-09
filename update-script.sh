390f78d991748fb3a7c74bb329b4b1d9839b0328
MarPiRK <marpirk@gmail.com>
2015-04-09 22:05:07 +0200

    update-script.sh now removes old documentation files to prevent from leaving unused files in tree

--- update-script.sh
+++ update-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /var/cache/poppler-cpp-doc/poppler-upstream
+cd /var/cache/poppler-cpp-doc/doc-repo/
+find . ! -name .git ! -name README.md ! -name update-script.sh -maxdepth 1 -type f -delete
+cd /var/cache/poppler-cpp-doc/poppler-upstream/
 git pull
 cd cpp
 rm -fr APIDOCS-html
