a4d517ecf2dd696daa238f63a4fe99d526879eed
MarPiRK <marpirk@gmail.com>
2015-04-13 12:13:45 +0200

    2015-04-13

--- update-script.sh
+++ update-script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-cd /var/cache/poppler-cpp-doc/doc-repo/
+BASEDIR=" /var/cache/doc-gen/poppler-cpp-doc"
+cd $BASEDIR/doc-repo/
 find . ! -name .git ! -name README.md ! -name update-script.sh -maxdepth 1 -type f -delete
-cd /var/cache/poppler-cpp-doc/poppler-upstream/
+cd $BASEDIR/poppler-upstream/
 git pull
 cd cpp
 rm -fr APIDOCS-html
 doxygen Doxyfile
-cp -fr APIDOCS-html/* /var/cache/poppler-cpp-doc/doc-repo/
-cd /var/cache/poppler-cpp-doc/doc-repo/
+cp -fr APIDOCS-html/* $BASEDIR/doc-repo/
+cd $BASEDIR/doc-repo/
 git add .
 git commit -m "`date +%Y-%m-%d`"
 git push
